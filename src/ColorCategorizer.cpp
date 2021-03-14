c62c6f441c12b1db0f8d85b100c45ff1f911a643
vadi2016 <vadi2016@outlook.com>
2021-03-14 14:57:48 +0300

    new structure

--- src/ColorCategorizer.cpp
+++ src/ColorCategorizer.cpp
@@ -1,96 +1,109 @@
 #include "ColorCategorizer.hpp"
-#define HISTOGRAM_SIZE 256
-#define ALPHABET_SIZE 256
-
-using namespace std;
 
 
-unsigned int histogram[HISTOGRAM_SIZE];
-int alphabet[ALPHABET_SIZE];
+int ExtractNum(std::fstream *file, char delimiter) {
+    int alphabet[256];
+    char sym;
+    int res = 0;
+    unsigned int digit_b = static_cast<unsigned int>(file->tellg());
 
-int main(int argc, char** args) 
-{
-    fstream f;
-    ofstream out("histogram.txt");
-    int width = 0;
-    int height = 0;
+    do {
+        file->read(&sym, 1);
+    } while (sym != delimiter);
+
+    int digit_e = static_cast<int>(file->tellg()) - 2;
+    int j = 0;
+    for (int d = '0'; d <= '9'; d++, j++) alphabet[d] = j;
+    int c = 1;
+    for (j = digit_e; j >= digit_b; j--, c *= 10) {
+        file->seekg(j);
+        file->read(&sym, 1);
+        res += alphabet[sym] * c;
+    }
+    file->seekg(digit_e + 2);
+    return res;
+}
+
+void BuildHistogram(std::fstream *file,
+                    unsigned int *histogram,
+                    unsigned int len,
+                    int width,
+                    int height) {
+    char sym;
+    for (unsigned int d = 0; d < len; ++d)histogram[d] = 0;
+    file->seekg(static_cast<unsigned int>(file->tellg()) + 4);
+    for (unsigned int d = 0; d < (width * height); ++d) {
+        if (file->eof())break;
+        file->read(&sym, 1);
+        histogram[static_cast<unsigned char>(sym)] += 1;
+    }
+    std::cout << std::endl;
+    for (int j = 0; j < 256; j++) {
+        std::cout << "[" << j << "] ";
+        for (unsigned int c = 0; c < histogram[j]; c++)
+            std::cout << "|";
+        std::cout << std::endl;
+    }
+}
+
+void PrintHistogram(const unsigned int *histogram,
+                    unsigned int len,
+                    int width,
+                    int height) {
+    auto *out = new std::ofstream("histogram.txt");
+    *out << "Building histogram for graphic file" << std::endl;
+    *out << "Width: " << width << "\t Height: " << height << std::endl;
+    for (unsigned int j = 0; j < len; j++) {
+        *out << "[" << j << "] ";
+        for (unsigned int c = 0; c < histogram[j]; c++)
+            *out << "|";
+        *out << std::endl;
+    }
+    *out << "End of file " << std::endl << std::endl;
+    out->close();
+    delete out;
+}
+
+void ProcessFile(std::fstream *file, unsigned int *histogram, unsigned int len) {
+    int width, height;
     char PGM_hdr1[2];
-    cout << "Program started" << endl;
+
+    file->read(PGM_hdr1, 2);
+    if ((PGM_hdr1[0] != 0x50) || (PGM_hdr1[1] != 0x35)) return;
+    std::cout << "PGM header detected" << std::endl;
+
+    file->seekg(static_cast<unsigned int>(file->tellg()) + 1);
+
+    width = ExtractNum(file, 0x20);
+    std::cout << "Width: " << width << std::endl;
+
+    height = ExtractNum(file, 0x0A);
+    std::cout << "Height: " << width << std::endl;
+
+    BuildHistogram(file, histogram, len, width, height);
+
+    PrintHistogram(histogram, len, width, height);
+}
+
+int main(int argc, char **args) {
+    auto *f = new std::fstream();
+    const unsigned int SIZE = 256;
+    unsigned int histogram[SIZE];
+
     if (argc == 1) {
-        cout << "Please specify input file(s)!" << endl;
+        std::cout << "Please specify input file(s)!" << std::endl;
         return 1;
     }
     for (int i = 1; i < argc; i++) {
-        f.open(args[i], ios::binary|ios::in);
-        if (!f.is_open())
-        {
-            cout << "File: " << args[i] << " is not accesible!" << endl;
+        f->open(args[i], std::ios::binary | std::ios::in);
+        if (!f->is_open()) {
+            std::cout << "File: " << args[i] << " is not accessible!" << std::endl;
             continue;
         }
-        cout << "Opened " << args[i] << " file" << endl;
-        f.read(PGM_hdr1, 2);
-        if ((PGM_hdr1[0] != 0x50)||(PGM_hdr1[1] != 0x35)) continue;
-        cout << "PGM header detected" << endl;
-        f.seekg(static_cast<unsigned int>(f.tellg()) + 1);
-        unsigned int digit_b = static_cast<unsigned int>(f.tellg());
-        char sym;
-        do
-        {
-           f.read(&sym, 1);
-        }while(sym != 0x20);
-        unsigned int digit_e = static_cast<unsigned int>(f.tellg()) - 2;
-        int j = 0;
-        for (int d = '0'; d <= '9'; d++, j++) alphabet[d] = j;
-        int c = 1;
-        for(j = digit_e; j >= digit_b; j--, c *= 10)
-        {
-            f.seekg(j);
-            f.read(&sym, 1);
-            width += alphabet[sym] * c;
-        }
-        cout << "Width: " << width << endl;
-        digit_b = digit_e + 2;
-        c = 1;
-        do
-        {
-            f.read(&sym, 1);
-        }while(sym != 0x0A);
-        digit_e = static_cast<unsigned int>(f.tellg()) - 2;
-        for (j = digit_e; j >= digit_b; j--, c *= 10)
-        {
-            f.seekg(j);
-            f.read(&sym, 1);
-            height += alphabet[sym] * c;
-        }
-        cout << "Height: " << height << endl;
-        for (int d = 0; d < 256; ++d) histogram[d] = 0;
-        f.seekg(digit_e + 6);
-        for (int d = 0; d < (width * height); ++d)
-        {
-            if (f.eof()) break;
-            f.read(&sym, 1);
-            histogram[static_cast<unsigned char>(sym)] += 1;
-        }
-        cout << endl;
-        for (j = 0; j < 256; j++)
-        {
-            cout << "[" << j << "] ";
-            for (c = 0; c < histogram[j]; c++) cout << "|";
-            cout << endl;
-        }
-        f.close();
-
-        out << "File: " << args[i] << endl;
-        out << "Width: " << width << "\tHeight: " << height << endl;
-        for (j = 0; j < 256; j++)
-        {
-            out << "[" << j << "] ";
-            for (c = 0; c < histogram[j]; c++) out << "|";
-            out << endl;
-        }
-        out << "End of file" << args[i] << endl << endl;
-        out.close();
+        std::cout << "File " << args[i] << " opened" << std::endl;
+        ProcessFile(f, histogram, SIZE);
+        f->close();
     }
-    cout << "Program ended" << endl;
-	return 0;
+    delete f;
+    return 0;
 }
